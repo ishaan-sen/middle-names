2361ae8cec05fb18a2fb70a1525501cc63b7076f
Ishaan Sen <ishaan.sen100@gmail.com>
2023-11-09 10:48:53 -0500

    Multi-word names

--- names.py
+++ names.py
@@ -4,9 +4,11 @@ from sys import argv
 from hashlib import sha256
 
 first_name = argv[1]
-last_name = argv[2]
-
-hash = sha256(bytes(first_name + last_name, 'utf-8')).hexdigest()
+last_name = argv[2:]
+# print(last_name)
+hash_name = first_name + ''.join(last_name)
+# print(hash_name)
+hash = sha256(bytes(hash_name, 'utf-8')).hexdigest()
 random.seed(int(hash[:16], 16))
 
 f = open("names.list")
@@ -18,7 +20,7 @@ def middle_name():
     first = random.choice(lines)
     last = random.choice(lines)
 
-    print(first, last)
+#     print(first, last)
 
     middle = last[:floor(len(last) / 2)] + first[floor(len(first) / 2):]
     return middle
@@ -28,6 +30,5 @@ full_name = first_name + ' '
 for i in range(random.randint(1, 3)):
     full_name = full_name + middle_name() + ' '
 
-full_name = full_name + last_name
-
+full_name = full_name + ''.join([i + ' ' for i in last_name])
 print(full_name)
